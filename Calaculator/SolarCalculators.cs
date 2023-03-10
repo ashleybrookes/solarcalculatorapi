9175b0dc10a25a01f73bccd5f288c32630363973
ashley <ashleybrookes86@gmail.com>
2023-03-10 18:53:17 +0000

    sunangle calc

---
+++ Calaculator/SolarCalculators.cs
@@ -0,0 +1,58 @@
+namespace SolarCalculator
+{
+    public class Calculator {
+
+        //need to store the data of all the values
+
+
+        public void GetSunAngle(LocationSolarData locationData ) {
+            
+        }
+
+
+    } 
+
+    public class AngleSunCalculator {
+            public DateTime dt;
+            public double JulianDay;
+
+            public double JulianCentury;
+            
+            public double GeomMeanLongSun;
+
+            public double GeomMeanAnomSun;
+            public double EccentEarthOrbit;
+            public double SunEqOfCtr;
+            public double SunTrueLong;
+            
+            public double SunTrueAnom;
+
+            
+            public double SunRadVector;
+
+
+            public double SunAppLong_deg;
+            public double MeanObliqEcliptic_deg;
+            public double ObliqCorr_deg;
+
+            public double SunRtAscen_deg;
+            public double SunDeclin_deg;
+            public double VarY;
+            public double EqOfTime_minutes;
+                	
+            public double HASunrise_deg;
+            	
+            public DateTime SolarNoon_LST;
+            public DateTime SunriseTime_LST;
+            public DateTime SunsetTime_LST;
+            public double SunlightDuration_minutes;
+            	public double TrueSolarTime_min;
+                public double HourAngle_deg;
+
+                public double SolarZenithAngle_deg;
+                public double SolarElevationAngle_deg)	Approx Atmospheric Refraction (deg)	Solar Elevation corrected for atm refraction (deg)	Solar Azimuth Angle (deg cw from N)
+
+
+    }
+
+}
